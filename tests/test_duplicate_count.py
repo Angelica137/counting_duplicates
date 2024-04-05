287c1062cda24400336e9da6c89331ca3e1effff
Angelica <Angelica137@users.noreply.github.com>
2024-04-05 16:27:02 +0100

    duplicate_count() returns three, green

--- tests/test_duplicate_count.py
+++ tests/test_duplicate_count.py
@@ -3,3 +3,7 @@ from src.duplicate_count import duplicate_count
 
 def test_duplicate_count_returns_one():
     assert duplicate_count("1") == 1
+
+
+def test_duplicate_count_returns_three():
+    assert duplicate_count("He11o") == 3
